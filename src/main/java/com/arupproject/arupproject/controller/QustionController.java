7c71791fedd2e5d73edb48665352708854b08fc3
Arup1996 <arup.sardar@indusnet.co.in>
2024-06-03 13:46:08 +0530

    change in qustion import

--- src/main/java/com/arupproject/arupproject/controller/QustionController.java
+++ src/main/java/com/arupproject/arupproject/controller/QustionController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
